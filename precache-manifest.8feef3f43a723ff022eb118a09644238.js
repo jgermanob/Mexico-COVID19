9f26fec06aeca7aa33c39ce4aa2f89b2eabb737b
Ricardo <ricardoj.epc@gmail.com>
2020-04-15 02:12:47 -0500

    Updates

---
+++ precache-manifest.8feef3f43a723ff022eb118a09644238.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a99987d01b1ff0292a59f3ef6c18d0d0",
+    "url": "/Mexico-COVID19/index.html"
+  },
+  {
+    "revision": "ea1604feca75741a52a7",
+    "url": "/Mexico-COVID19/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "967b18278a457ae1113f",
+    "url": "/Mexico-COVID19/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ea1604feca75741a52a7",
+    "url": "/Mexico-COVID19/static/js/2.a5616ddc.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/Mexico-COVID19/static/js/2.a5616ddc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "967b18278a457ae1113f",
+    "url": "/Mexico-COVID19/static/js/main.af8e233b.chunk.js"
+  },
+  {
+    "revision": "64d2012e3267e430e468",
+    "url": "/Mexico-COVID19/static/js/runtime-main.ac2cebaf.js"
+  }
+]);
